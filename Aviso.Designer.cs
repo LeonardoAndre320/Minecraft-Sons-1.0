c27763b0d652d26f7720259bdb3b0adad3527425
Leonardo <leonardoandre320@gmail.com>
2020-02-17 17:09:58 -0300

    Init

---
+++ Aviso.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Minecraft_Sons_1._0
+{
+    partial class Aviso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.bnt2 = new System.Windows.Forms.Button();
+            this.bnt1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(13, 13);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(47, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Titulo";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.ForeColor = System.Drawing.Color.White;
+            this.lblMensagem.Location = new System.Drawing.Point(14, 33);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(59, 13);
+            this.lblMensagem.TabIndex = 1;
+            this.lblMensagem.Text = "Mensagem";
+            // 
+            // bnt2
+            // 
+            this.bnt2.Location = new System.Drawing.Point(397, 63);
+            this.bnt2.Name = "bnt2";
+            this.bnt2.Size = new System.Drawing.Size(75, 23);
+            this.bnt2.TabIndex = 2;
+            this.bnt2.Text = "OK";
+            this.bnt2.UseVisualStyleBackColor = true;
+            this.bnt2.Click += new System.EventHandler(this.bnt2_Click);
+            // 
+            // bnt1
+            // 
+            this.bnt1.Location = new System.Drawing.Point(316, 63);
+            this.bnt1.Name = "bnt1";
+            this.bnt1.Size = new System.Drawing.Size(75, 23);
+            this.bnt1.TabIndex = 3;
+            this.bnt1.Text = "Não";
+            this.bnt1.UseVisualStyleBackColor = true;
+            this.bnt1.Click += new System.EventHandler(this.bnt1_Click);
+            // 
+            // Aviso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(484, 98);
+            this.Controls.Add(this.bnt1);
+            this.Controls.Add(this.bnt2);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Aviso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button bnt2;
+        private System.Windows.Forms.Button bnt1;
+    }
+}
---
+++ Aviso.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Sons_1._0
+{
+    public partial class Aviso : Form
+    {
+        public Aviso()
+        {
+            InitializeComponent();
+        }
+        bool Pergunta = false;
+
+        bool resposta = false;
+        bool respondido = false;
+        public static bool mostrar(string Titulo, string Mensagem, string SMG = "simples,media,grave", bool Pergunta = false, string RespostaPadrao = "sim/não")
+        {
+
+            Aviso A = new Aviso();
+            A.Pergunta = Pergunta;
+            A.lblTitulo.Text = Titulo;//mostra as mensagens
+            A.lblMensagem.Text = Mensagem;
+
+            switch(SMG)//escolhe a cor do fundo
+            {
+                case "simples":  A.BackColor = Color.SteelBlue; break;
+                case "media":  A.BackColor = Color.Goldenrod; break;
+                case "grave":  A.BackColor = Color.IndianRed; break;
+                default: A.BackColor = Color.Goldenrod; break;
+            }
+
+            if(Pergunta)
+            {
+                A.bnt1.Visible = true;
+                A.bnt2.Text = "Sim";
+
+                if (RespostaPadrao == "sim")
+                    A.bnt2.Focus();
+                if (RespostaPadrao == "não")
+                    A.bnt1.Focus();
+
+                while (!A.respondido) { }
+            }
+            else
+            {
+                A.bnt1.Visible = false;
+                A.bnt1.Enabled = false;
+
+                A.bnt2.Text = "OK";
+                while (!A.respondido) { }
+            }
+            return A.resposta;
+            A.Close();
+        }
+
+        private void bnt1_Click(object sender, EventArgs e)
+        {
+            Aviso A = new Aviso();
+            A.resposta = false;
+            A.respondido = true;
+        }
+
+        private void bnt2_Click(object sender, EventArgs e)
+        {
+            Aviso A = new Aviso();
+            if(A.Pergunta)
+            {
+                A.resposta = true;
+                A.respondido = true;
+            }
+            else
+            {
+                A.Close();
+            }
+        }
+    }
+}
