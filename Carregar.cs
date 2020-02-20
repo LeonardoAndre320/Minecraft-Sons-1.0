ab96f0f692789bb62932a8230d647167b8d6bc27
Leonardo <leonardoandre320@gmail.com>
2020-02-20 15:55:42 -0300

    Init

--- Carregar.cs
+++ Carregar.cs
@@ -7,83 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Minecraft_Sons_1._0
 {
     public partial class Carregar : Form
     {
-        private delegate void ProgressoDelegate(int progresso);
-        private ProgressoDelegate del;
-
-        public int Progresso
-        {
-            get { return Barra.Value; }
-            set { Barra.Value = value; }
-        }
-
-        public Carregar()
+        public Carregar(int Etapa, int Total)
         {
             InitializeComponent();
-            Barra.Maximum = 100;
-            del = AtualizacaoProgressoInterna;
-        }
 
-        private void AtualizacaoProgressoInterna(int progresso)
-        {
-            if(Handle == null)
-            {
-                return;
-            }
-            Invoke(del,progresso);
         }
-    }
-
-    public class HardWorker
-    {
-        public event EventHandler<HardWorkerEventArgs> ProgressChanged;
-        public event EventHandler HardWorkDone;
-        public void doHardWork()
-        {
-            for (int i = 1; i <= 100; i++)
-            {
-                for (int j = 1; j <= 500000; j++)
-                {
-                    Math.Pow(i, j);
-                }
-                OnProgressChanged(i);
-            }
-        }
-
-        private void OnProgressChanged(int Progress)
-        {
-            var handLer = ProgressChanged;
-            if (handLer != null)
-            {
-                handLer(this, new HardWorkerEventArgs(Progress));
-            }
-        }
-
-        private void OnHardWorkDone()
-        {
-            var handLer = HardWorkDone;
-            if (handLer != null)
-            {
-                handLer(this, EventArgs.Empty);
-            }
-        }
-
-        public class HardWorkerEventArgs : EventArgs
-        {
-            public HardWorkerEventArgs(int progress)
-            {
-                Progress = progress;
-            }
-            public int Progress
-            {
-                get;
-                private set;
-            }
-        }
-            
     }
 }
