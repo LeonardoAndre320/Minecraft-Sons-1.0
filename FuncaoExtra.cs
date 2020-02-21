d6e5189082fb4dbfa36e5174877ed926f9a4d4d6
Leonardo <leonardoandre320@gmail.com>
2020-02-21 15:59:45 -0300

    Init

--- FuncaoExtra.cs
+++ FuncaoExtra.cs
@@ -11,6 +11,35 @@ namespace Minecraft_Sons_1._0
     {
         public string[,] LerTextura(string Local)
         {
+            //Nome//Tipo//Local
+            string[,] Dados = new string[1,1];
+
+            #region Vendo se os arquivos existem
+            Dados[0, 0] = "s";Dados[0, 1] = "s";
+            if (!Directory.Exists(Local + "//assets//minecraft//sounds"))
+            {
+                Dados[0,0] = "n";
+            }
+            if (!File.Exists(Local + "//assets//minecraft//sounds.json"))
+            {
+                Dados[0, 1] = "n";
+                LerJson(Local + "//assets//minecraft//sounds.json");
+            }
+            #endregion
+
+            return Dados;
+        }
+
+        private string[,] LerJson(string LocalJson)
+        {
+            string[] Jsoni = File.ReadAllLines(LocalJson);//Lê todos as linhas
+            List<string> Json = new List<string>(Jsoni);//Converte para editar mais facil
+
+            Json.Remove("{");//remove para ficar só os dados
+            Json.Remove("}");
+
+            
+
             return null;
         }
     }
